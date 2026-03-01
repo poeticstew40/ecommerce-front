ec844c860204974b859cbc2215e83f378d5beb96
poeticstew40 <nicolas_gigena@hotmail.es>
2026-03-01 17:54:21 -0300

    Update api.js

--- src/tienda/services/api.js
+++ src/tienda/services/api.js
@@ -18,7 +18,7 @@ function normalizeBaseURL(url) {
 }
 
 // URL de producción por defecto
-const PROD_API_URL = 'https://clear-celestina-poeticstew40-82115c5d.koyeb.app/';
+const PROD_API_URL = 'https://ecommerce-back-phi.vercel.app/';
 
 let baseURL;
 if (import.meta.env.VITE_API_URL) {
